b7dfc147b812731550342363b9e24333b40bab8a
xmcb008@sina.com <xmcb008@sina.com>
2018-10-16 23:08:18 +0800

    JavaStudySample

---
+++ src/main/java/JavaSample/Samp019.java
@@ -0,0 +1,12 @@
+package JavaSample;
+
+public class Samp019 {
+    public static void main(String[] args){
+        System.out.println("manin()开始运行了");
+        //输出错误信息
+        System.err.println("在运行期间手动输出一个错误消息:");
+        System.err.println("\t 该软件没有买保险，请注意安全");
+        System.out.println("PrintErrorAndDebug.main()");
+        System.out.println("main()方法运行结束");
+    }
+}
---
+++ src/main/java/JavaSample/Samp020.java
@@ -0,0 +1,19 @@
+package JavaSample;
+
+import java.util.Scanner;
+
+public class Samp020 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("请输入你的身份证号：");
+
+        String line = scanner.nextLine();
+
+        System.out.println(line);
+
+
+
+    }
+}
